c8ab76719e24fd90c7a305da42c007ae1eeaac38
Terrence <78748177+terrencehj@users.noreply.github.com>
2022-09-26 23:17:20 -0400

    added PA2 files

---
+++ src/assignment/DriverPA2.java
@@ -0,0 +1,64 @@
+package assignment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DriverPA2 {
+	
+	private static int size;
+	static int[] inputArray; // Array elements to be modified and for generating MinHeap
+	static int[] newElementsArray; // New elements to be inserted in the Heap
+	
+	// Helper functions
+	static void prepareInputDataFromText(String filename) throws FileNotFoundException, IOException{
+			
+		File file = new File(filename);
+        Scanner sc = new Scanner(file);
+        
+        String size_n = sc.nextLine();
+       
+        size = Integer.parseInt(size_n);
+        
+        // Allocating the array for convenience
+        inputArray = new int[2*size + 10];
+        
+        String input = sc.nextLine();
+        String[] strs = input.trim().split("\\s+");
+        StringArrToIntArr(inputArray, strs, 1);
+        
+        input = sc.nextLine();
+        strs = input.trim().split("\\s+");
+        newElementsArray = new int[strs.length];
+        StringArrToIntArr(newElementsArray, strs, 0);
+        
+        sc.close();
+    }
+	
+	// Helper functions to populate array according to the need
+	public static void StringArrToIntArr(int[] arr, String[] s, int startingindex) {
+		
+		//startingindex is the index from where we are populating the array from input file
+		for (int i = 0, j = startingindex; i < s.length; i++, j++) {
+			arr[j] = Integer.parseInt(s[i]);
+	    }
+	}
+	
+	
+	public static void main(String[] arg) throws FileNotFoundException, IOException{
+	    
+		// Read the file and generate output
+		// Change the filename according to your need for testing purpose
+		String fileName = "minheap0.txt";
+		prepareInputDataFromText(fileName);
+		
+		MinHeap_Solution minHeapObj = new MinHeap_Solution();
+		
+		// This result will be evaluated in Gradescope
+		String[] generatedResults = minHeapObj.MinHeapOperations(inputArray, newElementsArray, size);
+		
+	}
+
+}
---
+++ src/assignment/MinHeap.java
@@ -0,0 +1,156 @@
+package assignment;
+
+public class MinHeap {
+	
+	static int[] minHeap;
+	static int size_n;
+	
+	
+	/**
+	 * Helper functions
+	 * MinHeapOperations will evaluate three operations: buildMinHeap, downHeap & upHeap
+	 * Parameters:
+	 * input: Integers from the Second line of input text
+	 * newelements: Integers from the Third line of input text
+	 * size: Input array size from the First line of input text
+	 * The operations to be implemented:
+	 * 1. buildMinHeap: Build the MinHeap
+	 * 2. downHeap: Algorithm downheap restores the heap-order property by swapping key k along a downward path from the root. 
+	 * 3. upHeap: The upheap process is used to add a node to a heap while maintaining the Heap property.
+	 * heapSort and printMinHeapArray helper functions are added for generating the desired output.
+	 */
+	public String[] MinHeapOperations(int[] input, int[] newelements, int size) {
+		
+		// Assigning/Initiating the class static members
+		minHeap = input.clone();
+		size_n = size;
+		
+		// These two variables are needed for Gradescope evaluation
+		String[] results = new String[3];
+		String removedElements = "";
+		
+		
+		buildMinHeap(minHeap, size_n);
+
+		
+		results[0] = printMinHeapArray(minHeap, size_n);
+		System.out.println("Result after buildheap: " + results[0]);
+		
+		
+		//Remove 5 minimum numbers from the MinHeap one by one.
+		for(int i = 0; i < 5; i++) {
+			removedElements = removedElements + removeMin(minHeap) + " ";
+		}
+		
+		results[1] = removedElements.trim();
+		System.out.println("Removed Elements: " + results[1]);
+		
+		
+		//Insert the Elements from the newelements one by one.
+		for (int i = 0; i < newelements.length; i++) {
+			insert(minHeap, newelements[i]);
+		}
+		
+		results[2] = heapSort(minHeap, size_n);
+		System.out.println("HeapSort result: " + results[2]);
+		
+		
+		return results;
+		
+	}
+	
+	// Helper function
+	/* A utility function for heapSort operation */
+	static String heapSort(int array[], int n)
+    {
+ 
+        // One by one extract an element from heap
+        for (int i = n; i > 0; i--) {
+             
+            // Move current root to end
+            int temp = array[1];
+            array[1] = array[i];
+            array[i] = temp;
+            
+            downHeap(array, 1, i);
+        }
+        
+        return printMinHeapArray(array, n);
+    }
+	
+	// Helper function
+	/* A utility function to print array of size n */
+	static String printMinHeapArray(int arr[], int n)
+    {
+        String result = "";
+    	for (int i = 1; i <= n; ++i)
+            result = result + arr[i] + " ";
+        
+    	return result.trim();
+    }
+	
+	/** 
+     * Helper function
+	 * Remove the minimum/smallest elements from the MinHeap
+     * During each removal process, ensure the MinHeap property.
+     * minHeap array gets updated during this process
+	 */
+    public static int removeMin(int[] array) {
+    	
+    	int popped = array[1];
+		array[1] = array[size_n];
+		size_n--;
+		
+		downHeap(array, 1, size_n);
+		
+		return popped;
+		
+	}
+    
+    /*
+     * Helper function
+     * Insert the elements one by one in the MinHeap
+     * Store the new item at the last empty cell in the array
+     * minHeap array gets updated during this process
+     */
+    public static void insert(int[] array, int newelement) {
+    	
+    	size_n++;
+	    int current = size_n;
+	    array[current] = newelement;
+	    
+	    upHeap(array, size_n);
+    }
+    
+	/**
+     * Build the MinHeap
+     * minHeap array gets updated during this process
+	 */
+    public static void buildMinHeap(int[] array, int size) {
+		
+    	/*** *** Write YOUR CODE HERE *** ***/
+		
+	}
+	
+    
+    /** 
+     * After replacing the root key with the key of the last node, the heap-order property may be violated
+	 * downHeap restores the heap-order  property by swapping key k along a downward path from the node at given index
+	 */
+    public static void downHeap(int arr[], int index, int size) {
+		
+		/*** *** Write YOUR CODE HERE *** ***/
+		
+	}
+    
+    /** 
+   	 * During each insertion process, ensure the MinHeap property.
+   	 * After the insertion of a new element, the heap-order property may  be violated. upHeap restores the heap-order property by swapping k along an upward path from the insertion node
+   	 */
+    public static void upHeap(int[] array, int size) {
+		
+		/*** *** Write YOUR CODE HERE *** ***/
+		
+	}
+
+}
